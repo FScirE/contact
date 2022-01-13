fe12a9f3c8ec8356d659de9b4b91e8f9a7b4e5e9
Eric <leo.soleke@gmail.com>
2022-01-13 15:12:00 +0100

    yeah

--- src/main/kotlin/Contact.kt
+++ src/main/kotlin/Contact.kt
@@ -1,6 +1,6 @@
 class Contact(var firstname: String, var surname: String, var phonenumber: MutableList<String>, var mail: MutableList<String>) {
     fun print() {
-        println("Firstname: $firstname \n Surname: $surname")
+        println("Firstname: $firstname\nSurname: $surname")
         var i = 1
         for (p in phonenumber) {
             println("Phonenumber $i: $p")
@@ -8,35 +8,72 @@ class Contact(var firstname: String, var surname: String, var phonenumber: Mutab
         }
         i = 1
         for (m in mail) {
-            println("Mail: $i: $m")
+            println("Mail $i: $m")
             i++
         }
     }
 }
 
-class ContactHandler {
-    private var contactlist: MutableList<Contact> = mutableListOf()
+class ContactHandler() {
+    var contactlist: MutableList<Contact> = mutableListOf()
 
     fun add() {
-        var firstname: String = ""
-        var surname: String = ""
+        var line = ""
+        var firstname = ""
+        var surname = ""
         var phonenumber: MutableList<String> = mutableListOf()
         var mail: MutableList<String> = mutableListOf()
 
-        firstname = "Eric"
-        surname = "Falkbring"
-        phonenumber.add("0725566809")
-        mail.add("leo.soleke@gmail.com")
+        print("Enter firstname: ")
+        line = readLine().toString()
+        firstname = line
+
+        print("Enter surname: ")
+        line = readLine().toString()
+        surname = line
+
+        print("Enter phone number: ")
+        phonenumber.add(readLine().toString())
+
+        while (true) {
+            print("Enter another phone number (empty to continue): ")
+            line = readLine().toString()
+            if (line == "") break
+            phonenumber.add(line)
+        }
+
+        print("Enter mail: ")
+        mail.add(readLine().toString())
+        while (true) {
+            print("Enter another mail (empty to continue): ")
+            line = readLine().toString()
+            if (line == "") break
+            mail.add(line)
+        }
 
         contactlist.add(Contact(firstname, surname, phonenumber, mail))
+    }
+
+    fun remove(): Boolean {
+        var index: Int = -1
+
+        print()
+
+        print("Which contact do you want to remove? (Number only): ")
+        index = readLine().toString().toInt()
+
+        if (index < 1 || index > contactlist.size) return false
+        contactlist.removeAt(index - 1)
+        return true
     }
 
     fun print() {
         var i = 1
         for (c in contactlist) {
-            println("Contact $i ------------------")
+            println("Contact $i ----------------------")
             c.print()
-            println("----------------------------- \n")
+            println("---------------------------------\n")
+            i++
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
-import java.net.ContentHandler
+var contactHandler = ContactHandler()
 
 fun main(args: Array<String>) {
+    contactHandler.contactlist.add(Contact("Eric", "Falkbring", mutableListOf<String>("0725566809"), mutableListOf("leo.soleke@gmail.com")))
+    contactHandler.contactlist.add(Contact("Olle", "Börner", mutableListOf<String>("0734161513"), mutableListOf("veisse.petric@gmail.com", "olle.petersson@utb.helsingborg.se")))
 
+    while (true) {
+        var choice = -1
+        println("==========================")
+        println("[1] Add contact\n[2] Remove contact\n[3] Print contacts")
+        println("==========================")
+        choice = readLine().toString().toInt()
+
+        if (choice == 1) {
+            contactHandler.add()
+            println("\nContact added!\n")
+        }
+        else if (choice == 2) {
+            var successful = contactHandler.remove()
+            if (successful) println("\nContact removed.\n")
+            else println("\nCouldn't remove contact.\n")
+        }
+        else if (choice == 3) {
+            contactHandler.print()
+        }
+    }
 }
